567bd9850663978b7a050fbf76e18d24ca456638
semsol23 <70558356+semsol23@users.noreply.github.com>
2020-11-15 22:57:18 +0900

    Update main.c

--- main.c
+++ main.c
@@ -8,31 +8,31 @@ int gameboard[N][N];
 
 int main(int argc, char *argv[]) {
 
-	int userInput[2];
-    int flipNum = 0, passCnt=0;
-    char turnColor[6] = "white";
+    int userInput[2];
+    int flipNum = 0, passCnt=0; /*초기값 설정*/
+    char turnColor[6] = "white"; /*첫번째 player는 흰돌*/
     int temp;
 
-	initOthello(gameboard);
+    initOthello(gameboard); /*게임판 초기화*/
     
-    while(isGameEnd(gameboard) == 0){
+    while(isGameEnd(gameboard) == 0){ /*아직 게임이 종료되지 않았을 때*/
         
-        printOthello(gameboard);
-        if(isCanNotPlay(turnColor, gameboard) == 1){
-            printf("[%s] can not place. Turn Change.\n",turnColor);
-            changeColor(turnColor);
-            passCnt++;
+        printOthello(gameboard); /*게임판 출력*/
+        if(isCanNotPlay(turnColor, gameboard) == 1){/*돌을 배치할 수 없을 때*/
+            printf("[%s] can not place. Turn Change.\n",turnColor);/*차례인 돌을 놓을 곳이 없어 반대색으로 차례 넘긴다고 출력*/
+            changeColor(turnColor);/*반대색 문자열로 변경*/
+            passCnt++;/*pass 횟수 +1*/
             continue;
-        }else{
+        }else{ /*돌을 배치할 수 있을 때*/
             passCnt = 0;
         }
-        if(passCnt > 1){
-            printf("White player and Black player can not play anymore.\n");
+        if(passCnt > 1){ /*pass 횟수가 2이상일 때*/
+            printf("White player and Black player can not play anymore.\n");/*두 플레이어가 모두 더이상 뒤집을 수 없을 때 더 이상 게임할 수 없음을 출력*/
             break;
         }
 
-        printf("\nput a new %s othello : ", turnColor);
-        scanf("%d %d", &userInput[0], &userInput[1]);
+        printf("\nput a new %s othello : ", turnColor);/*다음 차례의 플레이어 선언*/
+        scanf("%d %d", &userInput[0], &userInput[1]);/*1 3 의 형태로 입력한 좌표를 게임판 좌표로서 userInput에 대입*/
         
         if(validateUserInput(turnColor, userInput, gameboard) == 1){
             flipNum = doFlip(turnColor, userInput, gameboard);
